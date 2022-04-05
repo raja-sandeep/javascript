c984e15b460dc09b2a8749e03763f61712841792
raja-sandeep <rajasandeepvuba@gmail.com>
2022-04-05 18:16:29 +0530

    added strmethods file

---
+++ strMethods.js
@@ -0,0 +1,8 @@
+let x = "sandeep";
+console.log(x.charAt(1));
+console.log(x.charCodeAt());
+let y =" suman"
+var z ="satish"
+console.log(x.concat(y));
+console.log(x.indexOf("p"));
+console.log(x.search)
